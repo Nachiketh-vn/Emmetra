827905c24de8c3f686eb5cfa3d1f0314a681db45
Nachiketh-vn <nachikethnraddi@gmail.com>
2024-11-22 00:04:40 +0530

    first commit

--- components/clock.jsx
+++ components/clock.jsx
@@ -10,12 +10,35 @@ const AnalogClock = ({option1,option2,option3,option4}) => {
     }, 1000);
   }, []);
 
+  const [currentDate, setCurrentDate] = useState("");
+  const [currentTime, setCurrentTime] = useState("");
+
+  useEffect(() => {
+    // Function to update date and time
+    const updateDateTime = () => {
+      const now = new Date();
+      setCurrentDate(now.toLocaleDateString());
+      setCurrentTime(now.toLocaleTimeString());
+    };
+
+    // Initial call to set the date and time immediately
+    updateDateTime();
+
+    // Set interval to update time every second
+    const intervalId = setInterval(updateDateTime, 1000);
+
+    // Cleanup interval when component unmounts
+    return () => clearInterval(intervalId);
+  }, []);
+
   const clockStyles =
     option1 === "color1"
       ? "bg-[#164e63]" // Styling for color1
       : option1 === "color2"
       ? "bg-[#831843] " // Styling for color2
       : "bg-[#3C3C3C]"; // Default styling for current or others
+
+  const dateStyles = option2 === "digital" ? <p>{currentTime}</p> : option2 === "date" ? <p>{currentDate}</p> : "";
 
   return (
     <div className={`clock ${clockStyles}`}>
@@ -32,6 +55,7 @@ const AnalogClock = ({option1,option2,option3,option4}) => {
       <div className="hour nine">9</div>
       <div className="hour ten">10</div>
       <div className="hour eleven">11</div>
+      <div className="relative top-12 left-[2.8rem] text-sm py-[2px] bg-slate-800 font-semibold rounded-md text-center w-24">{dateStyles}</div>
       <div
         className="hour-hand"
         style={{
